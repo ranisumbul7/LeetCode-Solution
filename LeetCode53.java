21a894d6a72892e30d5bf7de87a14d2924220f47
Sumbul Rani <ranisumbul7@gmail.com>
2026-01-01 19:03:12 +0530

    Sum of Maximum sb Array (Medium)

--- LeetCode53.java
+++ LeetCode53.java
@@ -6,7 +6,7 @@ class Solution {
         for (int i = 1; i < nums.length; i++) {
             currentSum = Math.max(nums[i], currentSum + nums[i]);
             maxSum = Math.max(maxSum, currentSum);
-        }
+        }  
 
         return maxSum;
   
